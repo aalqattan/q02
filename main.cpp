d0d09c3c2d78f307702f3c257020f98f82f3c65b
aalqattan <aalqattan@mail.csuchico.edu>
2017-10-04 10:51:58 -0700

    q02

--- main.cpp
+++ main.cpp
@@ -7,7 +7,7 @@ using namespace std;
 int main()
 {
   vector <vector <string> > wordGrid;
-
+  vector <string> aQ;
   /*
      do not edit this space
 
@@ -25,7 +25,32 @@ int main()
   //of the element's row and column and the third blank should be replaced
   //with the value of that element
 
-  
+aQ.push_back("car");
+aQ.push_back("man");
+aQ.push_back("laptop");
+aQ.push_back("pin");
+aQ.push_back("book");
+
+wordGrid.push_back(aQ);
+
+aQ.clear();
+
+aQ.push_back("read");
+aQ.push_back("write");
+aQ.push_back("sleep");
+aQ.push_back("open");
+
+wordGrid.push_back(aQ);
+
+for(int r=0; r < wordGrid.size();r++) // r= row and c= column
+{
+for(int c=0; c < aQ.size();c++)
+{
+cout<<"Row "<<r<<"Column "<<c<<"is  "<<wordGrid[r][c]<<endl;
+}
+cout<<endl;
+
+
 
 
 
